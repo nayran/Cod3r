4949c65215dcf5b21646f0eb7d726a710a418549
Nayran Ziebell <nayranziebell@icloud.com>
2025-04-25 11:07:33 -0300

    Cod3r

---
+++ frontend/src/main.ts
@@ -0,0 +1,14 @@
+// https://www.cod3r.com.br/courses/angular-9-essencial
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
